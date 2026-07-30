ae4f23921361d5be43e92c0c8a00777ce33c7c62
0958946887aa <leokung45@gmail.com>
2026-07-30 20:39:58 +0700

    bababab

--- login.py
+++ login.py
@@ -5045,7 +5045,7 @@ def gacha_free_mode(device, cycle_start, serial, original_name, file_path, coin_
                     break
             miss_count += 1
             gui_log(serial, f"[Loop {loop_num}] gachafree1 not here, swiping... ({miss_count}/{max_miss})", step="Swipe")
-            device.shell("input swipe 618 308 54 306 3500")
+            device.shell("input swipe 618 308 54 306 6000")
             time.sleep(2.0)
 
         if not found_free:
